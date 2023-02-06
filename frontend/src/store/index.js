4cd5d883edde16b1c38853ce331ecdfa4c01b926
alexmil-sk <alexmilweb@gmail.com>
2023-02-06 14:36:05 +0100

    end with vuex

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -6,11 +6,9 @@ import download_icon from "../assets/icons/download.png";
 import join_right from "../assets/mode/join_right_red.png";
 import join_left from "../assets/mode/join_left_green.png";
 import logo from "../assets/logo/logo.png";
-import {
-	fetchMovies
-} from "../utils/fetchMovies.js";
-
-
+import actions from "./actions.js";
+import mutations from "./mutations.js";
+import getters from "./getters.js";
 
 export default createStore({
 	state() {
@@ -29,88 +27,7 @@ export default createStore({
 			chosenMovies: [],
 		}
 	},
-	mutations: {
-		isLoading(state) {
-			state.isLoading = true;
-		},
-		notIsLoading(state) {
-			state.isLoading = false;
-		},
-		changeMode(state) {
-			state.isChanged = !state.isChanged;
-		},
-		openModalPopup(state) {
-			state.isOpenModal = true;
-		},
-		closeModalPopup(state) {
-			state.isOpenModal = false;
-		},
-		deleteFilmsArray(state) {
-			state.films = [];
-		},
-		deleteChosenMoviesArray(state) {
-			state.chosenMovies = [];
-		},
-		deleteChosenItem(state, payload) {
-
-			const selectedMovie = state.chosenMovies.find(
-				(item) => item.id === payload
-			);
-			const isExist = state.films.find((item) => item.id === payload);
-
-			state.chosenMovies = state.chosenMovies.filter(
-				(item) => item.id !== payload
-			);
-
-			if (!isExist) {
-				state.films.push(selectedMovie);
-				localStorage.setItem("films", JSON.stringify(state.films));
-			}
-		},
-		goModalPopup(state) {
-			state.films = [];
-			this.commit('closeModalPopup');
-			this.commit('isLoading');
-		},
-		fetchMoviesMutation(state, payload) {
-			fetchMovies(payload.start, payload.end)
-				.then(res => state.films = res)
-		},
-		chooseFilm(state, payload) {
-			state.chosenMovies = state.chosenMovies.concat(
-				state.films.filter((item) => item.id === payload)
-			);
-			state.films = state.films.filter(
-				(item) => item.id !== payload);
-		}
-	},
-	actions: {
-		fetchMoviesAsync(context, payload) {
-			context.commit('fetchMoviesMutation', payload);
-			context.commit('notIsLoading');
-		}
-	},
-	getters: {
-		isChanged(state) {
-			return state.isChanged;
-		},
-		isOpenModal(state) {
-			return state.isOpenModal;
-		},
-		isLoading(state) {
-			return state.isLoading;
-		},
-		films(state) {
-			return state.films;
-		},
-		chosenMovies(state) {
-			return state.chosenMovies;
-		},
-		modalNumberStart(state) {
-			return state.modalNumberStart;
-		},
-		modalNumberEnd(state) {
-			return state.modalNumberEnd;
-		}
-	}
+	mutations,
+	actions,
+	getters
 });
